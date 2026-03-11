89282d924e1550d08158a98dfb99b552aa7c1ca9
KeirPar <keirparnitzke@gmail.com>
2026-03-10 19:02:47 -0700

    changed the schema for restaurant.py to make it more optimal, also delted resturant.py my other teamate made it and i didnt need it.

--- backend/app/schemas/restaurant.py
+++ backend/app/schemas/restaurant.py
@@ -1,29 +1,35 @@
 from pydantic import BaseModel
-from list import List
+from typing import List, Optional
 
-
+#dont have everything in the base model because we want to be able to update only certain fields without having to provide all of them
 class RestaurantBase(BaseModel):
-    id: int
     name: str
     address: str
-    cuisine_type : str
+    cuisine_type: str
+    phone_number: str
+
+#create and update models inherit from the base model, but the update model has all fields optional so we can update only certain fields without having to provide all of them
+class restaurantCreate(RestaurantBase):
+    pass
+    
+#but here we can update only certain fields without having to provide all of them, so we make all fields optional
+class RestaurantUpdate(RestaurantBase):
+    name: Optional[str] = None
+    address: Optional[str] = None
+    cuisine_type: Optional[str] = None
+    phone_number: Optional[str] = None
+    is_open: Optional[bool] = None
+
+#for reading
+class Restaurant(RestaurantBase):
+    id: int
     rating: float
     is_open: bool
-    phone_number: str
-    
-class RestaurantCreate(RestaurantBase):
-    id: int
-    name: str
-    address: str
-    phone_number: str
 
-class RestaurantUpdate(RestaurantBase):
-    id: int
-    name: str
-    address: str
-    phone_number: str
-
-
+#this will be for feature 3
+class SearchResponse(BaseModel):
+    message: str
+    data: List[Restaurant]
 
     
 
--- backend/app/schemas/resturant.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Resturant(BaseModel):
-    id: int
-    name: str
-    address: str
-    cuisine_type: str
-    rating: float
-    is_open: bool
