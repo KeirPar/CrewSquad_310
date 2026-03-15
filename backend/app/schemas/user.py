07c2482d5d99e4f7b68d53a0d2c57924fba0743c
BrandenSkoupas <brandenskoupas@gmail.com>
2026-03-15 12:52:17 -0700

    Updated user schema with UserRole for registration of users

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,8 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from enum import Enum
 
+#Making roles with enforcing of being a Customer or Owner exclusively
+class UserRole(str, Enum):
+    CUSTOMER = "Customer"
+    OWNER = "Restaurant Owner"
+
+#Using basemodel this is what users will be filling out on a creation/registration form
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    phone_number: str
+    password: str = Field(..., min_length=8)
+    role: UserRole
+
+#Storing the user data internally
 class User(BaseModel):
     id: int
     name: str
     password_hash: str
     email: str
     phone_number: str
+    role: UserRole
+
