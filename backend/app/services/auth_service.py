295f2255c048b2713ee78cadc86e86c1596e09a1
BrandenSkoupas <brandenskoupas@gmail.com>
2026-03-15 15:23:56 -0700

    Refactored to use Bcrypt directly to fix errors and make hashing possible

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,4 +1,4 @@
-from passlib.context import CryptContext
+import bcrypt
 from fastapi import HTTPException, status
 import jwt
 from datetime import datetime, timedelta, timezone
@@ -6,20 +6,26 @@ from datetime import datetime, timedelta, timezone
 SECRET_KEY = "secret_crewsquad_key"
 ALGORITHM = "HS256"
 
-#Setup Bcrypt hashing enginge
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
 class AuthService:
     @staticmethod
     def hash_password(password: str) -> str:
-        """Turn plain text password into hash"""
-        return pwd_context.hash(password)
+        """Turn plain text password into Bcrypt hash"""
+        pwd_bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(pwd_bytes, salt)
+        #Return as a string so it can be stored in db
+        return hashed.decode('utf-8')
     
     @staticmethod
     def verify_password(plain_password: str, hashed_password: str) -> bool:
-        """Checks if typed password matches hash"""
-        return pwd_context.verify(plain_password, hashed_password)
+        """Checks if typed password matches Bcrypt hash"""
+        try:
+            return bcrypt.checkpw(
+                plain_password.encode('utf-8'), 
+                hashed_password.encode('utf-8')
+            )
+        except Exception:
+            return False
 
     @staticmethod
     def create_access_token(data: dict):
@@ -31,13 +37,6 @@ class AuthService:
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt
 
-    @staticmethod
-    def verify_password(plain_password: str, hashed_password: str):
-        """
-        Simulated check. Will update later for real hashing real hashing.
-        """
-        return hashed_password == plain_password + "_fake_hash"
-    
     @staticmethod
     def get_current_user_role(token: str):
         """Decodes current JWT to find user's role."""
