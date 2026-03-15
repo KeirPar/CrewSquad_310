c62739e696d73d3b84d7fd5c163cdff966e69aad
KeirPar <keirparnitzke@gmail.com>
2026-03-14 20:44:53 -0700

    Made changes to the order_router, there is now a patch endpoint where you can update an order to change the status, however, now that Im looking at it and using it. I think it would make more sense if we used enums and not strings, i think ill change this next.

--- backend/app/routers/order_router.py
+++ backend/app/routers/order_router.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from app.schemas.cart import Cart
 from app.services.order_service import create_order
+from app.schemas.order import Order
+from app.schemas.customer import Customer
+from app.schemas.restaurant_manager import RestaurantManager
+from app.services.order_service import update_order_status, create_order
+from app.schemas.user import User
 
 router = APIRouter()
+
+def get_current_user() -> User: # because we dont have feat1 setup
+    # same as the fake user from the test file
+    return Customer( #change this to a restaurant manager if you want to test that instead
+        id=1,
+        name="Keir P",
+        password_hash="hashthingy", 
+        email="fake@gmail.com",
+        phone_number="604-677-6767",
+        default_address="123 Fake St", #gonna have to change if this is a restaurant manager
+    )
 
 @router.post("/orders")
 def place_order(cart: Cart):
@@ -18,5 +34,22 @@ def place_order(cart: Cart):
     """
     try:
         return create_order(order_id=1, cart=cart)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.patch("/orders/{order_id}/status")
+def change_order_status(
+    order_id: int,
+    order: Order, 
+    new_status: str, 
+    current_user: User = Depends(get_current_user) #fake user until we have feat1 setup
+):
+    try:
+        updated_order = update_order_status(
+            order=order, 
+            new_status=new_status, 
+            current_user=current_user
+        )
+        return {"message": "Order status updated successfully", "data": updated_order}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
