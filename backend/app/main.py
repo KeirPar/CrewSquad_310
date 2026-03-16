8d7b89b21cd7282cfb46115104160ccf3d3d9a6f
BrandenSkoupas <brandenskoupas@gmail.com>
2026-03-15 21:50:30 -0700

    Finalizing API endpoints by backing sure that routers are correctly used in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import search, order_router, auth_router, menu_router, cart_router #import the routers we need in our app
+from app.routers import search, order_router, auth_router, menu_router, cart_router, restaurant_router #import the routers we need in our app
 
 app = FastAPI(title = "CrewSquad_310 Backend") #create our FastAPI app
 
@@ -8,6 +8,8 @@ app.include_router(order_router.router) #include the order router in our app
 app.include_router(auth_router.router)#including authorization router in our app
 app.include_router(menu_router.router)#include menu router in our app
 app.include_router(cart_router.router)#include cart router in our app
+app.include_router(restaurant_router.router)#include restaurant router in our app
+
 
 @app.get("/")
 def read_root(): #just getting the root endpoint to return a simple message
