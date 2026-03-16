9f7a8b1ef88e69a292b33060c267bba1dd040dce
KeirPar <keirparnitzke@gmail.com>
2026-03-16 16:57:51 -0700

    changed some comments on the restaurant schema, had some repetitive stuff.

--- backend/app/schemas/restaurant.py
+++ backend/app/schemas/restaurant.py
@@ -9,7 +9,7 @@ class RestaurantBase(BaseModel):
     phone_number: str
     price_tier: int #for feat3-fr1, im gonna add this for our searching by price tier feature, this will be an integer from 1 to 3, with 1 being the cheapest and 3 being the most expensive
 
-#create and update models inherit from the base model, but the update model has all fields optional so we can update only certain fields without having to provide all of them
+#create and update models inherit from the base model
 class restaurantCreate(RestaurantBase):
     pass
     
@@ -20,7 +20,7 @@ class RestaurantUpdate(RestaurantBase):
     cuisine_type: Optional[str] = None
     phone_number: Optional[str] = None
     is_open: Optional[bool] = None
-    price_tier: Optional[int] = None #added this for the update model as well, so we can update the price tier if we want to
+    price_tier: Optional[int] = None #added this for the update model as well, so we can update the price tier
 
 #for reading
 class Restaurant(RestaurantBase):
