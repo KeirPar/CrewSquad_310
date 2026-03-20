8408c085ee30c45f550c5d249e48be2c132f9a8e
Zijian <256862361+human0102@users.noreply.github.com>
2026-03-20 12:27:50 -0700

    feat: get container recyling fee

--- backend/testing-documents/test_fees.py
+++ backend/testing-documents/test_fees.py
@@ -1,7 +1,8 @@
 import pytest
-from app.services.taxes.tax_rate_calculator import get_province_tax
-from app.services.taxes.tax_type import TaxType
-from app.services.taxes.province_tax import ProvinceTax
+from app.services.fees_calculator.get_province_tax import get_province_tax
+from app.services.fees_calculator.tax_type import TaxType
+from app.services.fees_calculator.province_tax import ProvinceTax
+from app.services.fees_calculator.get_container_recycling_fee import get_container_recycling_fee
 
 #   Test getting the total tax rate and separate tax rates by a valid province code.
 def test_tax_rates():
@@ -17,3 +18,7 @@ def test_tax_rates_with_fake_province_code():
     with pytest.raises(KeyError):
         province_code = "FAKE"
         province_tax = get_province_tax(province_code)
+
+
+def test_get_container_recycling_fee():
+    assert get_container_recycling_fee() == 0.1
