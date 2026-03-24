bec01796e0a262398694cc3adad74d3c8d11249d
Zijian <256862361+human0102@users.noreply.github.com>
2026-03-24 16:27:09 -0700

    feat: sort restaurants by user's location

--- backend/app/routers/search_router.py
+++ backend/app/routers/search_router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from typing import Optional             #import all the stuff we need for the router and the service
 from app.schemas.restaurant import SearchResponse, CuisineType
 from app.services.search_service import SearchService
+from app.services.auth_service import AuthService, oauth2_scheme
 from app.schemas.restaurant_sort_order import RestaurantSortOrder
+from app.schemas.user import User
 
 router = APIRouter(prefix="/search", tags=["Search"]) #make sure the router is setup
 search_service = SearchService()
@@ -14,15 +16,23 @@ def search_restaurants(
     min_rating: Optional[float] = None,
     sort_by: Optional[RestaurantSortOrder] = None, #how we're gonna sort (price or rating)
     limit: int = 10, #search return limit
-    offset: int = 0 #search return offset 
-    
+    offset: int = 0 #search return offset
+    # user: User = Depends(AuthService.get_current_user)
 ):
+    #   Get current user
+    user: Optional[User]
+    try:
+        user = AuthService.get_current_user(token=oauth2_scheme)
+    except Exception as e:
+        user = None
+    
     # Pass the query parameters down to the service layer
     return search_service.filter_restaurants(
         name=name, 
         cuisine_type=cuisine_type, 
         min_rating=min_rating,
         sort_by=sort_by, #added this for the sorting
+        from_coordinate=user.coordinate if user is not None else None,
         limit=limit, #added these 2 parameters to the service call
         offset=offset
     )
--- backend/app/services/search_service.py
+++ backend/app/services/search_service.py
@@ -1,13 +1,24 @@
 from app.repositories.restaurant_repo import RestaurantRepository #import our repository
 from app.schemas.restaurant_sort_order import RestaurantSortOrder
 from app.schemas.restaurant import CuisineType
+from app.packages.geo.coordinate import Coordinate
+from typing import Optional
 
 class SearchService: #make our SearchService class
 
     def __init__(self):
         self.repo = RestaurantRepository()
 
-    def filter_restaurants(self, name: str = None, cuisine_type: CuisineType = None, min_rating: float = None, sort_by: RestaurantSortOrder = None, limit: int = 10, offset: int = 0):
+    def filter_restaurants(
+        self, 
+        name: str = None, 
+        cuisine_type: CuisineType = None, 
+        min_rating: float = None, 
+        sort_by: RestaurantSortOrder = None, 
+        from_coordinate: Optional[Coordinate] = None, 
+        limit: int = 10, 
+        offset: int = 0
+    ):
         restaurants = self.repo.load_all() #get the data from the json
         if name: #name filter
             restaurants = [r for r in restaurants if name.lower() in r.get("name", "").lower()]
@@ -18,12 +29,20 @@ class SearchService: #make our SearchService class
         if min_rating is not None: #rating filter
             restaurants = [r for r in restaurants if r.get("rating", 0.0) >= min_rating]
 
+        sort_error_message = ""
         #added for sorting, we can sort by rating or price
         if sort_by == RestaurantSortOrder.RATING_DESC:
             restaurants.sort(key=lambda r: r.get("rating", 0.0), reverse=True) #sort by rating, high to low
         elif sort_by == RestaurantSortOrder.PRICE_ASC:
             restaurants.sort(key=lambda r: float(r.get("price_tier", 9999)), reverse=False) #sort by price, low to high
-
+        elif sort_by == RestaurantSortOrder.DISTANCE_ASC:
+            if from_coordinate is None:
+                sort_error_message = "No user coordinate provided, failed to sort restaurant by distance"
+            else:
+                restaurants.sort(
+                    key=lambda r: float(Coordinate(**r.get("coordinate")).get_kilometer_distance_to(from_coordinate)) if r.get("coordinate") is not None else 9999999999999,
+                    reverse=False
+                ) #sort by price, low to high
 
         paginated_restaurants = restaurants[offset:offset + limit] 
 
@@ -31,6 +50,13 @@ class SearchService: #make our SearchService class
             return { #if the list is empty, return a message saying no restaurants found matching criteria, and an empty list for data
                 "message": "No restaurants found matching your search. Try adjusting your filters or checking for typos.",  #I just made a small change to this message to make it a bit more helpful
                 "data": []
+            }
+        
+        #   When there's a sort error, return restaurants with the sort error message.
+        if sort_error_message != "":
+            return {
+                "message": sort_error_message,
+                "data": paginated_restaurants
             }
 
         #but if we made it here that means we have some results to return, so we return a success message and the paginated list of restaurants
