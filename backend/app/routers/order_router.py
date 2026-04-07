2055e79b89a470422bc4b71563a1d941ff3fde2c
KeirPar <keirparnitzke@gmail.com>
2026-04-06 21:53:44 -0700

    Added a frontend folder with a basic html, css and js setup for some of the backend featuers (search, orders, menu). I also made changes to the docker-compose file and created a new dockerfile for the frontend. Now there is a seperate container for the backend and frontend. I then made a small change to one of the endpoints in the order router.

--- backend/app/routers/order_router.py
+++ backend/app/routers/order_router.py
@@ -93,8 +93,8 @@ def get_order_status(order_id: int):
     if not order:
         raise HTTPException(status_code=404, detail="Order not found")
     
-    payment = payment_db.find_by_order_id(order_id)
-    return {"order": order, "payment": payment}
+    return {"order status": order.status} #fixed to just return order status
+
 
 @router.patch("/{order_id}/status")
 def change_order_status(
